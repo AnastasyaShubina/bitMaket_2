ad68371d7de1f9c3377328fea528b4103834a8a7
Nikita Tkach @F0RES <i@ntkach.ru>
2023-11-08 12:22:18 +0700

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ import { onDocumentReady, importAll } from './functions.js'
 import { modal } from './modal'
 import { sliderCharts } from './sliderCharts'
 import { sliderAbout } from './sliderAbout'
-import { animation, animation2 } from './animation'
+import { animation } from './animation'
 import { modalContactUs } from './modalContactUs'
 
 
@@ -20,33 +20,71 @@ onDocumentReady(function () {
   console.log('hello.')
 })
 
-
-
 var currentPageUrl = window.location.href;
 if (currentPageUrl.includes('about')) {
   sliderAbout()
   animation()
+  const dateId = document.querySelector('#date')
+  const date = new Date();
+  dateId.textContent = date.toLocaleDateString('en-EN', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).toUpperCase();
+  var header = document.querySelector('.header');
+  header.classList.add('blackHead');
   // animation2()
 } else {
   sliderCharts()
-  
+  Chart.defaults.font.family = 'Milligram';
+  Chart.defaults.borderColor = '#3D3D3D';
+  Chart.defaults.color = '#3D3D3D';
+
+
+  function formatDate(date) {
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    day = (day < 10) ? '0' + day : day;
+    month = (month < 10) ? '0' + month : month;
+    return day + '/' + month;
+  }
+
   function updateDates() {
     var currentDate = new Date();
     var nextDate = new Date(currentDate);
 
-    var text1 = document.getElementById("text1");
-    text1.textContent = currentDate.toDateString();
+    for (var i = 1; i <= 6; i++) {
+      var endDate = new Date(nextDate.getTime() + 6 * 24 * 60 * 60 * 1000);
 
-    for (var i = 2; i <= 6; i++) {
-      nextDate.setDate(nextDate.getDate() + 1);
       var currentText = document.getElementById("text" + i);
-      currentText.textContent = nextDate.toDateString();
+      currentText.textContent = formatDate(nextDate) + ' - ' + formatDate(endDate);
+
+      nextDate.setDate(nextDate.getDate() + 7);
     }
   }
-  // Этот код будет выполнен при загрузке SVG
-  // Здесь мы вызываем функцию updateDates
+
   updateDates();
+
 }
+
+var blackElements = document.querySelectorAll('.change-black-element'); // Получаем все элементы с классом 'change-black-element'
+var header = document.querySelector('.header');
+var classChange = 'blackHead';
+
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY; // Получаем позицию скролла
+
+    header.classList.remove(classChange); // Удаляем класс, если он уже присвоен
+
+    blackElements.forEach(function(element) {
+        var blackBlockTop = element.getBoundingClientRect().top + window.scrollY; // Получаем начало блока
+        var blackBlockBottom = blackBlockTop + element.offsetHeight; // Получаем конец блока
+
+        if (scrollPosition >= blackBlockTop && scrollPosition <= blackBlockBottom) {
+            header.classList.add(classChange);
+        }
+    });
+});
 
 modal()
 modalContactUs()
--- src/js/sliderCharts.js
+++ src/js/sliderCharts.js
@@ -1,180 +1,117 @@
+import { accum, config, data, down, up } from "./chart";
+
+
+
 export const sliderCharts = () => {
-  const myBlock = document.querySelector('.chart-container');
-  const blockWidth = myBlock.getBoundingClientRect().width;
 
-  const startDate = new Date();
-  const numPoints = 30;
-  const initialPrice = 0.0000015;
-  const volatility = 0.4;
-  const updateInterval = 600;
-  const duration = 0;
-  const svg = d3.select("#chart");
-  svg.attr('width', blockWidth);
-  const width = +svg.attr("width");
-  const height = +svg.attr("height");
-  const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
-  const data = [];
+  let myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+  let myChart2 = new Chart(
+    document.getElementById('myChart2'),
+    config
+  );
+  let myChart3 = new Chart(
+    document.getElementById('myChart3'),
+    config
+  );
+  const today = new Date(); // Получаем сегодняшнюю дату
+
+
+  const swiper = new Swiper(".sliderCharts", {
+    centeredSlides: true,
+    spaceBetween: 30,
+    slidesPerView: 1,
+  });
+
   const btn1Slider = document.querySelector('.btn1-slider')
+  btn1Slider.addEventListener('click', function (e) {
+    myChart.destroy();
+    data.length = 0;
+    const now = new Date(); // Получаем текущую дату и время
+
+    for (let i = 0; i < 100; i++) {
+      const date = new Date(now);
+      date.setMinutes(now.getMinutes() + i * 15); // Увеличиваем дату на i * 15 минут
+      data.push({ x: date, y: up[i] });
+    }    
+
+    myChart3.data.datasets[0].borderColor = '#0FFBE4';
+    myChart = new Chart(
+      document.getElementById('myChart'),
+      config
+    );
+
+    e.preventDefault();
+    swiper.slideTo(0, 0);
+  });
+
   const btn2Slider = document.querySelector('.btn2-slider')
+  btn2Slider.addEventListener('click', function (e) {
+    myChart2.destroy();
+    data.length = 0;
+    const now = new Date(); // Получаем текущую дату и время
+
+    for (let i = 0; i < 100; i++) {
+      const date = new Date(now);
+      date.setMinutes(now.getMinutes() + i * 15); // Увеличиваем дату на i * 15 минут
+      data.push({ x: date, y: accum[i] });
+    }    
+
+    myChart3.data.datasets[0].borderColor = '#FF7A00';
+    myChart2 = new Chart(
+      document.getElementById('myChart2'),
+      config
+    );
+
+    e.preventDefault();
+    swiper.slideTo(1, 0);
+  });
+
   const btn3Slider = document.querySelector('.btn3-slider')
+  btn3Slider.addEventListener('click', function (e) {
+    myChart3.destroy();
+    data.length = 0;
+    const now = new Date(); // Получаем текущую дату и время
 
-  let count = 0
-  let dataChart = [];
-  let pricesData = generatePriceData(startDate, numPoints, initialPrice, volatility, "accumulation");
-  let interval = setInterval(() => { addDataPoint(pricesData) }, updateInterval);
+    for (let i = 0; i < 100; i++) {
+      const date = new Date(now);
+      date.setMinutes(now.getMinutes() + i * 15); // Увеличиваем дату на i * 15 минут
+      data.push({ x: date, y: down[i] });
+    }
 
-  const xScale = d3.scaleLinear().range([0, innerWidth]);
-  const yScale = d3.scaleLinear().range([innerHeight, 0]);
-  const line = d3.line().x((d, i) => xScale(i)).y(d => yScale(d));
+    myChart3.data.datasets[0].borderColor = '#FF3628';
+    myChart3 = new Chart(
+      document.getElementById('myChart3'),
+      config
+    );
+    e.preventDefault();
+    swiper.slideTo(2, 0);
+  });
 
-  btn2Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn2-slider active'
+  btn1Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider active'
 
-  // Generate initial data points
-  data.push(...pricesData);
+  swiper.on('slideChange', function (e) {
+    if (e.activeIndex === 0) {
 
-  const g = svg
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+      btn1Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider active'
 
-  g.append("g")
-    .attr("class", "y-axis")
-    .attr("color", `#3D3D3D`)
-    .call(d3.axisLeft(yScale));
-
-  g.append("g")
-    .attr("class", "grid")
-    .attr("transform", `translate(0,${innerHeight})`)
-    .call(d3.axisBottom(xScale).tickSize(-innerHeight).tickFormat(''))
-
-  g.append("g")
-    .attr("class", "grid")
-    .call(d3.axisLeft(yScale).tickSize(-innerWidth).tickFormat(''))
-
-  const path = g
-    .append("path")
-    .datum(data)
-    .attr("class", "line")
-    .attr("fill", "none")
-    .attr("stroke-width", "2px")
-    .attr("stroke", "#FF7A00");
-
-  xScale.domain([0, numPoints - 1]);
-  yScale.domain([d3.min(data), d3.max(data)]);
-
-  function addDataPoint(pricesData) {
-    data.push(pricesData[count]);
-    data.shift();
-
-    xScale.domain([data.length - numPoints, data.length - 1]);
-    yScale.domain([d3.min(data), d3.max(data)]);
-
-    // Update the line with smooth transitions and translate it to the left
-    path.transition()
-      .duration(duration / 2) // Duration for the transition
-      .attr("d", line)
-      .attr("transform", null) // Reset the transform
-      .transition()
-      .ease(d3.easeLinear)
-      .attr("transform", `translate(${xScale(0)}, 0)`); // Translate the line to the left
-
-    // Update the x-axis and y-axis with smooth transitions
-    g.select(".x-axis")
-      .transition()
-      .duration(duration / 2)
-      .call(d3.axisBottom(xScale));
-
-    g.select(".y-axis")
-      .transition()
-      .duration(duration / 2)
-      .call(d3.axisLeft(yScale));
-
-
-    dataChart.push(pricesData[count]);
-    count += 1;
-    // Check if we need to regenerate data
-    if (!pricesData[count]) {
-      clearInterval(interval);
-      count = 0;
-      const _pricesData = generatePriceData(startDate, numPoints, dataChart[dataChart.length - 1], volatility, "accumulation");
-      interval = setInterval(() => { addDataPoint(_pricesData) }, updateInterval);
-      path.attr("stroke", "#FF7A00");
-
-      btn1Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider'
-      btn2Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider active'
+      btn2Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider'
       btn3Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn2-slider'
     }
-  }
+    if (e.activeIndex === 1) {
+      btn2Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn2-slider active'
 
-  btn1Slider.addEventListener('click', function (e) {
-    e.preventDefault();
-    clearInterval(interval);
-    count = 0;
-    const _pricesData = generatePriceData(startDate, numPoints, dataChart[dataChart.length - 1], volatility, "aggressive");
-    console.log(_pricesData)
-    interval = setInterval(() => { addDataPoint(_pricesData) }, updateInterval);
-    path.attr("stroke", "#0FFBE4");
-
-    btn1Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider active'
-    btn2Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider'
-    btn3Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn2-slider'
-  });
-
-  btn2Slider.addEventListener('click', function (e) {
-    e.preventDefault();
-    clearInterval(interval);
-    count = 0;
-    const _pricesData = generatePriceData(startDate, numPoints, dataChart[dataChart.length - 1], volatility, "accumulation");
-    console.log(_pricesData)
-    interval = setInterval(() => { addDataPoint(_pricesData) }, updateInterval);
-    path.attr("stroke", "#FF7A00");
-
-    btn1Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider'
-    btn2Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider active'
-    btn3Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn2-slider'
-  });
-
-  btn3Slider.addEventListener('click', function (e) {
-    e.preventDefault();
-    clearInterval(interval);
-    count = 0;
-    const _pricesData = generatePriceData(startDate, numPoints, dataChart[dataChart.length - 1], volatility, "profit");
-    console.log(_pricesData)
-    interval = setInterval(() => { addDataPoint(_pricesData) }, updateInterval);
-    path.attr("stroke", "#FF3628");
-
-    btn1Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider'
-    btn2Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider'
-    btn3Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn2-slider active'
-  });
-
-};
-
-
-// Function to generate price data with more small jumps during growth and fall
-function generatePriceData(startDate, numPoints, initialPrice, volatility, forecastScenario) {
-  const priceData = [];
-  // let currentDate = new Date(startDate);
-
-  for (let i = 0; i < numPoints; i++) {
-    const priceChange = (Math.random() - 0.5) * 2 * volatility;
-    let newPrice = initialPrice + priceChange;
-
-    if (forecastScenario === "aggressive") {
-      const growthModifier = Math.random() < 0.6 ? 0.02 : 0;
-      newPrice += newPrice * (0.02 + growthModifier);
-    } else if (forecastScenario === "accumulation") {
-      // No additional price changes during accumulation level
-    } else if (forecastScenario === "profit") {
-      const fallModifier = Math.random() < 0.6 ? -0.02 : 0;
-      newPrice += newPrice * (-0.02 + fallModifier);
+      btn1Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider'
+      btn3Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn3-slider'
     }
+    if (e.activeIndex === 2) {
+      btn3Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn3-slider active'
 
-    priceData.push(newPrice);
-    // currentDate.setMinutes(currentDate.getMinutes() + 30);
-    initialPrice = newPrice; // Update the initialPrice for the next iteration
-  }
+      btn1Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn1-slider'
+      btn2Slider.className = 'firstBlock--container__schedule--buttons__bgButtons btn2-slider'
+    }
+  });
 
-  return priceData;
 }
